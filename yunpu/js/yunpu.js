813ff1aa6e4554e8bd66ce5f927430afc55d323e
liure <liure1127@163.com>
2016-05-02 00:40:50 +0800

    Signed-off-by: liure <liure1127@163.com>

--- yunpu/js/yunpu.js
+++ yunpu/js/yunpu.js
@@ -1,11 +1,5 @@
-/*
-* @Author: liurq
-* @Date:   2016-03-28 20:28:01
-* @Last Modified by:   liurq
-* @Last Modified time: 2016-03-29 23:39:09
-*/
 
-'use strict';
+
 $(function(){
 	$('.headerR li').click(function(e) {
 		$(this).addClass('first').siblings().removeClass('first')
